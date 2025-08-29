2c67cfb1ddb29b96d82ce0d3caa1ca8ccc0d0def
Xinyue Cui <zoey88889@gmail.com>
2025-08-29 17:42:24 +0800

    chore: deploy 2025-08-29 17:42:24

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
-cat > deploy.sh <<'EOF'
 #!/usr/bin/env bash
 set -e
 branch=${1:-main}
 git add -A
 git commit -m "chore: deploy $(date +%F' '%T)"
 git push -u origin "$branch"
-EOF
-chmod +x deploy.sh
